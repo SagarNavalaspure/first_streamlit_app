090afe68ea1ce3d8b77026bf38bd03c2e3dfbfbc
Sagar Navalaspure <118710131+SagarNavalaspure@users.noreply.github.com>
2022-11-21 11:47:22 +0530

    Update first_streamlit_app.py
    adding menu

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('my parents new healthy diner')
+streamlit.header ('Breakfast menu')
+STREAMLIT.text('omega 3 and blueberry oatmeal')
+STREAMLIT.text('kale,spinach & rocket smoothie')
+STREAMLIT.text('hard boiled free range egg')
